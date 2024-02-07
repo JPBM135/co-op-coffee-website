92a5c8593a86d3a7e50591a92a97bcc6d08a18ae
JPBM135 <jpedrobm0@gmail.com>
2024-02-06 20:05:09 -0800

    feat(landing): ratings section

--- src/script.js
+++ src/script.js
@@ -38,9 +38,9 @@ $(document).ready(function () {
     slidesToShow: 3,
     slidesToScroll: 3,
     prevArrow:
-      '<button type="button" class="slick-prev main__menu__carousel__button"><i class="ph ph-caret-circle-left"></i></button>',
+      '<button type="button" class="slick-prev main__ratings__carousel__button"><i class="ph ph-caret-circle-left"></i></button>',
     nextArrow:
-      '<button type="button" class="slick-next main__menu__carousel__button"><i class="ph ph-caret-circle-right"></i></button>',
+      '<button type="button" class="slick-next main__ratings__carousel__button"><i class="ph ph-caret-circle-right"></i></button>',
     responsive: [
       {
         breakpoint: 1024,
@@ -61,5 +61,25 @@ $(document).ready(function () {
         settings: "unslick", // destroys slick
       },
     ],
+  });
+
+  const randomStarsSources = $(".main__ratings__carousel__item__stars");
+
+  randomStarsSources.each(function () {
+    const randomStars = Math.max(Math.floor(Math.random() * 5) + 1, 3);
+    let stars = [];
+    for (let i = 0; i < randomStars; i++) {
+      stars.push(
+        '<i class="ph-fill ph-star main__ratings__carousel__item__stars__star--selected"></i>'
+      );
+    }
+
+    for (let i = 0; i < 5 - randomStars; i++) {
+      stars.push(
+        '<i class="ph-fill ph-star main__ratings__carousel__item__stars__star--unselected"></i>'
+      );
+    }
+
+    $(this).html(stars);
   });
 });
