8018187e9733e4220672fcdf4ee082d89591f465
JPBM135 <jpedrobm0@gmail.com>
2024-02-06 18:08:24 -0800

    feat(landing): coffee carousel

--- src/script.js
+++ src/script.js
@@ -1,7 +1,65 @@
-$('#landing-menu-carrousel').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true
+$(document).ready(function () {
+  $("#landing-menu-carousel").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow:
+      '<button type="button" class="slick-prev main__menu__carousel__button"><i class="ph ph-caret-circle-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-next main__menu__carousel__button"><i class="ph ph-caret-circle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 300,
+        settings: "unslick", // destroys slick
+      },
+    ],
+  });
+
+  $("#landing-menu-ratings").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow:
+      '<button type="button" class="slick-prev main__menu__carousel__button"><i class="ph ph-caret-circle-left"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-next main__menu__carousel__button"><i class="ph ph-caret-circle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 300,
+        settings: "unslick", // destroys slick
+      },
+    ],
+  });
 });
